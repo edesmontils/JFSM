664842bf33d6f1255c5b8a0bfb50643bca1892d9
edesmontils <emmanuel.desmontils@univ-nantes.fr>
2017-09-23 10:35:16 +0200

    ed

--- JFSM.java
+++ JFSM.java
@@ -47,69 +47,18 @@ import java.util.HashMap;
 
 import java.util.Iterator;
 
-class Etat {
-	public String name;
-	public boolean isInitial, isFinal;
-
-	public Etat(String n) {
-		this.name = n;
-		isInitial = false;
-		isFinal = false;
-	}
-
-	public void action(Context ctx) {}
-}
-
-class Trash extends Etat {
-	public Trash(){
-		super("---Trash---");
-	}
-}
-
-class Transition {
-	public String name;
-	public String source, cible;
-	public String symbol;
-
-	public Transition(String s, String symbol, String c) {
-		this.symbol = symbol;
-		this.source = s;
-		this.cible = c;
-		this.name = s+"-"+symbol+"->"+c;
-	}
-
-	public Transition(String s, String c) {
-		this(s,"",c);
-	}
-
-	public boolean isEpsilon(){
-		return symbol == null;
-	}
-
-	public void action(Context ctx) {}
-}
-
-class EpsilonTransition extends Transition {
-	public EpsilonTransition(String s, String c) {
-		super(s,"",c);
-	}
-}
-
-class Context {
-
-}
-
 abstract class Automate {
 	protected Map<String,Etat> Q;
 	protected Set<String> F, I;
 	protected Set<String> A;
+	protected List<String> histo;
+	//protected Map<String,Map<String,Transition>> mu2;
 	Set<Transition> mu;
-	Context ctx;
 	String current;
 	int statut;
 	Etat trash;
 
-	public Automate(Set<String> A, Set<Etat> Q, Set<Transition> mu, Context ctx) {
+	public Automate(Set<String> A, Set<Etat> Q, Set<String> I, Set<String> F, Set<Transition> mu) {
 		assert A.size()>0 : "A ne peut pas être vide" ;
 		this.A = A;
 		this.mu = new HashSet<Transition>();
@@ -141,13 +90,27 @@ abstract class Automate {
 
 		this.mu.addAll(mu);
 
+		// this.mu2 = new HashMap<String,Map<String,Transition>>();
+		// Iterator<Transition> j = mu.iterator();
+		// while(j.hasNext()) {
+		// 	Transition t = j.next();
+		// 	String s = t.source+t.symbol ;
+		// 	Map<String,Transition> m = this.mu2.get(s);
+		// 	if (m == null) {
+		// 		this.mu2.put(s, new HashMap<String,Transition>());
+		// 		this.mu2.get(s).put(t.name, t);
+		// 	} else {
+		// 		if (m.containsKey(t.name)) {
+		// 			System.out.println("Transition dupliquée ! Seule la première version sera conservée.");
+		// 		} else {
+		// 			m.put(t.name,t);
+		// 		}				
+		// 	}
+		// }
+
 		this.I = new HashSet<String>();
 		this.F = new HashSet<String>();
-
-		this.ctx = ctx;
 	}
-
-	public Automate(Set<String> A, Set<Etat> Q, Set<Transition> mu) {this(A,Q,mu,new Context());}
 
 	public void addTransition(Transition t) {
 		mu.add(t);
@@ -193,20 +156,31 @@ abstract class Automate {
 		histo.clear();
 	}
 
+	public boolean next(String symbol) {
 		histo.add(current);
+		return true;
 	}
 
+	public boolean accepte(){return F.contains(current);} // pas suffisent !!!!!
 
 	public abstract boolean run(String s, String sep) ;
 }
 
 class AFD extends Automate {
 	private boolean estDeterministe;
+	private String i;
 
-	public AFD(Set<String> A, Set<Etat> Q, Set<Transition> mu, String i,  Context ctx){
-		super(A,Q,mu,ctx);
-		setInitial(i);
+	public AFD(Set<String> A, Set<Etat> Q, String i, Set<String> F, Set<Transition> mu){
+		super(A,Q,F,new HashSet<String>(),mu);
+		I.add(i);
+		this.i = i;
 		assert testDeterminisme(this) : "L'automate doit être déterministe";
+		init();
+	}
+
+	public void init() {
+		super.init();
+		current = i;
 	}
 
 	static public boolean testDeterminisme(Automate T) {
@@ -218,6 +192,8 @@ class AFD extends Automate {
 		// return true;
 	}
 
+	public boolean next(String symbol) {
+		return super.next(symbol);
 
 	}
 	public boolean run(String s, String sep) {
@@ -243,6 +219,8 @@ public class JFSM {
     	mu.add(new Transition("2","b","2"));
     	mu.add(new Transition("3","b","2"));
 
-    	afn.setFinal("3");
+    	Set<String> F = new HashSet<String>();
+    	F.add("3");
+    	Automate afn = new AFD(Ae, Q, "1", F, mu);
    }
 }
